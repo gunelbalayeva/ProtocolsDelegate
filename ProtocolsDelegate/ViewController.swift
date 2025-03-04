69768ef1966e97cf29a1c3e93ee6c63637c69a1c
gunelbalayeva <gunelbalayeva97@gmail.com>
2025-03-04 11:45:17 +0400

    Initial Commit

---
+++ ProtocolsDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProtocolsDelegate
+//
+//  Created by User on 04.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
